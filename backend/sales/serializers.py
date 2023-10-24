eea6f34ca5c37e1f5da7f08487cf77384910b4fe
Mavisousalima <mavisousalima@gmail.com>
2023-10-24 16:19:44 -0300

    fix: Get sale returning only the id of the product

--- backend/sales/serializers.py
+++ backend/sales/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from .models import Sale
+from products.serializers import ProductSerializer
 
 
 class SaleSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
     class Meta:
         model = Sale
         fields = '__all__'
